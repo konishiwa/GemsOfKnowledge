6267ca470b7b88bcafee9273e7d65d195be6dca2
Ryan Ammons <ryan@ammonsdatasolutions.com>
2018-02-03 22:28:48 -0500

    Initial commit with user authentication

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "home#index"
+
+end
