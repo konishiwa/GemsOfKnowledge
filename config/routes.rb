b88538223bc97c93958e8e0c0b9f20b1c4ee105f
Ryan Ammons <ryan@ammonsdatasolutions.com>
2018-02-13 01:59:52 -0500

    Minor changes to routes.rb--notes on forcing routing reload in debug.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root to: 'quizzes#index'
+
   resources :quizzes do
     get 'take'
-    patch 'grade' => "quizzes#grade"
+    patch 'grade' => 'quizzes#grade'
   end
 
-  resources :questions
+  # resources :questions # DO NOT make questions individually accessible via url
 
   devise_for :users
 
-  root to: "quizzes#index"
+  # NOTES:
+  # Use Rails.application.reload_routes! in paused debug console to force routes to reload
 
 end
